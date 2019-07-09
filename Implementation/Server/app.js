6cd354c1c7c5a1d816a258b76171c053e6e93251
Dominik König <dominik_klaus_pascal.koenig@smail.th-koeln.de>
2019-07-09 16:12:14 +0200

    Add files via upload

--- Implementation/Server/app.js
+++ Implementation/Server/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 //Here we are calling our installed modules and assigning them to variable express and bodyParser respectively
 
-var routes = require("./routes.js");
+var routes = require("./routes/routes.js");
 var app = express();
 
 app.use(bodyParser.json());
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 routes(app);
 
-var server = app.listen(3333, function () {
-    console.log("Dienstgeber running on port:", server.address().port);
+var server = app.listen(3000, function () {
+    console.log("app running on port", server.address().port);
 })
 //Here we defined the port where our server should be running on
